55bf79421f1b6c371db780b9ec1c7e7b48cacc14
Haris <velixvgo@gmail.com>
2026-07-15 16:35:17 +0500

    implement functionality to convert dec to binary

--- DecimalToBinary/main.cpp
+++ DecimalToBinary/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+void getBitValue(int number, int bitValue)
+{
+	int q{ number / bitValue };
+	std::cout << (q % 2) ? "1" : "0";
+}
+
 int main()
 {
+	std::cout << "Enter a number (0 - 255): ";
+	int number{};
+	std::cin >> number;
 
-	std::cout << "Hello, world!\n";
+	getBitValue(number, 128);
+	getBitValue(number, 64);
+	getBitValue(number, 32);
+	getBitValue(number, 16);
+	getBitValue(number, 8);
+	getBitValue(number, 4);
+	getBitValue(number, 2);
+	getBitValue(number, 1);
+
 	return 0;
-
 }
