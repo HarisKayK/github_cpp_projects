7134a5877d83d4d37a772403d6d06be4cad6bb11
HarisKayK <HarisKayK@gmail.com>
2026-07-31 22:33:37 +0500

    Document the code

--- FractionProduct/Functions.cpp
+++ FractionProduct/Functions.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include "Fraction.h"
 
+// This functions asks user for numerator and denominator for
+// a Fraction and stores them in the strcut's 
+// numerator and denominator member variables
 Fraction getFraction()
 {
 	Fraction temp_f{};
 
 	std::cout << "Enter numerator: ";
 	std::cin >> temp_f.numerator;
-
 	std::cout << "Enter denominator: ";
 	std::cin >> temp_f.denominator;
+
+	std::cout << '\n';
 
 	return temp_f;
 }
 
+// This function finds the product of numerators of f1 and f2
+// Finds the profuct of denominators of f1 and f2
+// Initializes a new struct with the products as numerator and denominator
 Fraction FractionProduct(const Fraction& f1, const Fraction& f2)
 {
 	Fraction temp
@@ -25,6 +32,8 @@ Fraction FractionProduct(const Fraction& f1, const Fraction& f2)
 	return temp;
 }
 
+// This function prints the numerator and denominator of the
+// final Profuct fraction
 void PrintFraction(const Fraction& f)
 {
 	std::cout << f.numerator << "/" << f.denominator;
--- FractionProduct/main.cpp
+++ FractionProduct/main.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
-#include "Fraction.h"
-#include "Functions.h"
+#include "Fraction.h"  // for struct Fraction
+#include "Functions.h" // for getFraction(), PrintFraction(), Fractionroduct()
+#include <iostream>    // for std::cout
 
 int main()
 {
-	Fraction fraction_one{ getFraction() };
-	std::cout << '\n';
-	Fraction fraction_two{ getFraction() };
+	// Get Fractions from user
+	Fraction f1{ getFraction() };
+	Fraction f2{ getFraction() };
 
-	PrintFraction(FractionProduct(fraction_one, fraction_two));
+	// Print the product of f1 and f2
+	PrintFraction(FractionProduct(f1, f2));
 
 	return 0;
 }
